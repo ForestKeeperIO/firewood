295b32849c1a7e791b8aac1a042f126e6002c2b7
Determinant <tederminant@gmail.com>
2022-10-24 17:59:36 -0700

    more doc; change the name of `DBHeader` and `MerkleHeader`

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,8 @@
 //! In firewood, we take a closer look at the second regime and have come up with a simple but
 //! solid design that fulfills the need for such blockchain storage.
 //!
+//! ## Storage Model
+//!
 //! Firewood is built by three layers of abstractions that totally decouple the
 //! layout/representation of the data on disk from the actual logical data structure it mains:
 //!
@@ -93,7 +95,103 @@
 //!
 //! The three layers are depicted as follows:
 //!
-//! ![Three Layers of Firewood](../figures/three-layers.svg)
+//! <p align="center">
+//!     <img src="https://drive.google.com/uc?export=view&id=1KnlpqnxkmFd_aKZHwcferIdX137GVZJr" width="80%">
+//! </p>
+//!
+//! Given the abstraction, one can easily realize the fact that the actual data that affects the
+//! state of the data structure (MPT) is what the linear space (`MemStore`) keeps track of, that is,
+//! a flat but conceptually large byte vector. In other words, given a valid byte vector as the
+//! content of the linear space, the higher level data structure can be *uniquely* determined, there
+//! is nothing more (except for some auxiliary data that are kept for performance, such as caching)
+//! or less than that, like a way to view the bytes. This nice property allows us to completely
+//! separate the logical data from its physical representation, and greatly simplifies the storage
+//! code. It is also very generic and versatile, as in theory any persistent data could be stored
+//! this way -- sometimes you need a different `MemShale` or `MemStore` implementation, but you do
+//! not even have to touch the code for the persisted data structure.
+//!
+//! ## Page-based Shadowing and Revisions
+//!
+//! Following the idea that the MPTs are just a view of a linear byte space, all writes made to the
+//! MPTs inside Firewood will eventually be consolidated into some interval writes to the linear
+//! space. The writes may overlap and some frequent writes are even done to the same spot in the
+//! space. To reduce the overhead and be friendly to the disk, we partition the entire 64-bit
+//! virtual space into pages (yeah it appears to be more and more like an OS) and keep track of the
+//! dirty pages in `MemStore` (see `storage::StoreRevMut` which implements `MemStore`). When a
+//! [`db::WriteBatch`] commits, both the recorded interval writes and the aggregated in-memory
+//! dirty pages induced by this write batch are taken out from the linear space. Although they are
+//! mathematically equivalent, interval writes are more compact (suppose there are not a lot of
+//! overlaps) than pages (which are 4K in size, become dirty even if a single byte is touched upon). So
+//! interval writes are fed into the WAL subsystem (supported by `growthring`). After the WAL
+//! record is written (one record per write batch), the dirty pages are then pushed to the on-disk
+//! linear space to mirror the change by some asynchronous, out-of-order file writes. See the
+//! `BufferCmd::WriteBatch` part of `DiskBuffer:: process` for the detailed logic.
+//!
+//! In short, a Read-Modify-Write (RMW) style normal operation flow is as follows in Firewood:
+//!
+//! - Traverse the MPT, and that induces the access to the nodes. Suppose the nodes are not already in
+//!   memory, then:
+//!
+//! - Bring the necessary pages that contains the accessed nodes into the memory and cache them
+//!   (`storage::CachedSpace`).
+//!
+//! - Make changes to the MPT, and that induces the writes to the nodes. The nodes are either
+//!   already cached in memory (either its pages are cached, or its handle `ObjRef<Node>` is still in
+//!   `shale::ObjCache`) or need to bring into the memory (if that's the case, go back to second step for it).
+//!
+//! - Writes to nodes are converted into interval writes to the stagging `StoreRevMut` space that
+//!   overlays atop `CachedSpace`, so all dirty pages during the current write batch will be
+//!   exactly captured in `StoreRevMut` (see `StoreRevMut::take_delta`).
+//!
+//! - Finally:
+//!
+//!   - If the write batch is dropped without invoking `db::WriteBatch::commit`, all in-memory
+//!     changes will be aborted, the dirty pages from `StoreRevMut` will be dropped and the merkle
+//!     will "revert" back to its original state without having to actually change anything.
+//!
+//!   - Otherwise, the write batch is committed, the interval writes (`storage::Ash`) will be bundled
+//!     into a single WAL record (`storage::AshRecord`) and sent to WAL subsystem, before dirty pages
+//!     are scheduled to be written to the space files. Also the dirty pages are applied to the
+//!     underlying `CachedSpace` and `StoreRevMut` becomes empty again for further write batches.
+//!
+//! Parts of the following diagram depicts this normal flow, the "staging area" (implemented by
+//! `StoreRevMut`) concept is a bit similar to Git and that allows handling of (resuming from)
+//! operational errors and clean abort of an on-going write batch made to the entire store state.
+//! Essentially, we copy-on-write pages in the space that are touched upon, without directly
+//! mutating the underlying master space revision. The staging space is just a collection of these
+//! "shadowing" pages and a reference to the its base (master) so any reads could partially hit those
+//! dirty pages or just fall through to the base, whereas all writes are captured. Finally, when
+//! things go well, we "push down" these changes to the base and clear up the staging space.
+//!
+//! <p align="center">
+//!     <img src="https://drive.google.com/uc?export=view&id=1l2CUbq85nX_g0GfQj44ClrKXd253sBFv" width="100%">
+//! </p>
+//!
+//! Thanks to the shadow pages, we can both revive some historical revision of the store and
+//! maintain a rolling window of past revisions on-the-fly. The diagram shows previously logged
+//! write batch records could be kept even though they are no longer needed for the purpose of
+//! crash recovery. The interval writes from a record can be aggregated into pages (see `storage::
+//! StoreDelta::new`) and used to reconstruct a "ghost" image of past revision of the linear space
+//! (just like how staging space works, except that writes will be ignored so the ghost space is
+//! essentially read-only). The shadow pages there will function as some "rewinding" changes to
+//! patch the necessary locations in the linear space, while the rest of the linear space is
+//! very likely untouched by that historical write batch.
+//!
+//! Then, with the three-layer abstraction we previously talked about, an historical MPT could be
+//! derived. In fact, because there is no mandatory traversal or scanning for this process, the
+//! only cost to revive a historical state from the log is to just playback the records and create
+//! those shadow pages. There is very little cost because the ghost space is invoked on an
+//! on-demand manner while one accesses the historical MPT.
+//!
+//! In the other direction, when new write batches are committed, the system moves forward, we can
+//! also maintain a rolling window of past revisions in memory with *zero* cost. The bottom of the
+//! diagram shows when a write batch is committed, the persisted space goes one step forward,
+//! the staging space is cleared, and an extra ghost space can be created to hold the version of
+//! the store before the commit. The backward delta is applied to counteract the change that is
+//! made to the persisted store, which is also a set of shadow pages. No change is required for
+//! other historical ghost space instances. Finally, we can phase out some very old ghost space to
+//! keep the size invariant of the rolling window.
+//!
 pub(crate) mod account;
 pub mod db;
 pub(crate) mod file;
